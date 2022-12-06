8bb792a836dcd43e399c55691dee33479ce91978
jihaneh <jijih@hotmail.fr>
2022-12-06 13:14:00 +0100

    add seeder

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# HUNGRY
+Hunger.destroy_all
+Hunger.create(value: "Very hungry")
+Hunger.create(value: "Not very hungry")
+Hunger.create(value: "Not hungry")
+
+# CATEGORY
+Category.destroy_all
+
+# RESTAURANT
+Restaurant.destroy_all
+
+# IMAGER
+Imager.destroy_all
+
+# UBER
+Uber.destroy_all
+
+# MEAL
+Meal.destroy_all
+
+
