5f0ae1778fc5e1c948d389bee783c63781da21f7
jihaneh <jijih@hotmail.fr>
2022-12-07 17:51:51 +0100

    Update meal

--- app/serializers/meal_serializer.rb
+++ app/serializers/meal_serializer.rb
@@ -1,9 +1,9 @@
 class MealSerializer < ActiveModel::Serializer
-  attributes :id, :title, :price, :description, :image_url, :category, :hunger
-  belongs_to :restaurant
+  attributes :id, :title, :price, :description, :image_url, :category, :hunger, :rating_value, :rating_count,
+             :currency_code
 
-  class RestaurantSerializer < ActiveModel::Serializer
-    attributes :currency_code
+  def currency_code
+    object.restaurant.currency_code
   end
 
   def category
@@ -12,5 +12,13 @@ class MealSerializer < ActiveModel::Serializer
 
   def hunger
     object.categories.first.hunger.value
+  end
+
+  def rating_value
+    object.restaurant.uber.rating_value
+  end
+
+  def rating_count
+    object.restaurant.uber.rating_count
   end
 end
