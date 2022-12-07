dbd94e37bbec57efe7a2e260e24e97b9fa929b89
jihaneh <jijih@hotmail.fr>
2022-12-07 11:53:14 +0100

    Add fetch meal by budget

--- app/controllers/api/v1/meals_controller.rb
+++ app/controllers/api/v1/meals_controller.rb
@@ -1,6 +1,11 @@
 class Api::V1::MealsController < ApplicationController
   def index
-    result = Meal.all
+    min_budget = params['minBudget'] || 0
+    max_budget = params['maxBudget'] || 10_000_000
+    result = Meal
+      .where('price >= :min_budget AND price <= :max_budget', min_budget:, max_budget:)
+      .where(desactivate: false).order('RANDOM()')
+
     render json: { message: ['Meal list fetched successfully'],
                    status: 200,
                    type: 'Success',
