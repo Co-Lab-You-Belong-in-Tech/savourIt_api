f31f808e39090ab7f0ae44b4e2ba9045b8559aa3
jihaneh <jijih@hotmail.fr>
2023-01-12 09:13:32 +0100

    Add uber/door link in meal

--- app/serializers/meal_serializer.rb
+++ app/serializers/meal_serializer.rb
@@ -1,6 +1,6 @@
 class MealSerializer < ActiveModel::Serializer
   attributes :id, :title, :price, :description, :image_url, :category, :hunger, :rating_value, :rating_count,
-             :currency_code
+             :currency_code, :restaurant, :uber_url, :doordash_link, :delivery_time
 
   def currency_code
     object.restaurant.currency_code
@@ -20,5 +20,25 @@ class MealSerializer < ActiveModel::Serializer
 
   def rating_count
     object.restaurant.uber.rating_count
+  end
+
+  def restaurant
+    object.restaurant.name
+  end
+
+  def delivery_time
+    if object.restaurant.doordash.nil?
+      ''
+    else
+      object.restaurant.doordash.time_delivery
+    end
+  end
+
+  def doordash_link
+    if object.dash_link
+      object.restaurant.doordash.url
+    else
+      ''
+    end
   end
 end
